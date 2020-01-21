f4da838888054c5906f21f975e712adcdbb73b93
karolkrawczyk <karol.krawczyk787@gmail.com>
2020-01-21 14:21:39 +0100

    test

--- scripts/build/publish-chart-repo.sh
+++ scripts/build/publish-chart-repo.sh
@@ -34,10 +34,10 @@ helm repo index --url "https://$(dirname "$GITHUB_PAGES_REPO").github.io/$(basen
 
 # publish
 
-git -C "$GITHUB_PAGES_DIR" config user.email "travis@users.noreply.github.com"
-git -C "$GITHUB_PAGES_DIR" config user.name travis
-git -C "$GITHUB_PAGES_DIR" config core.sshCommand 'ssh -i github_deploy_key'
-git -C "$GITHUB_PAGES_DIR" add .
-git -C "$GITHUB_PAGES_DIR" status
-git -C "$GITHUB_PAGES_DIR" commit -m "Published by Travis"
-git -C "$GITHUB_PAGES_DIR" push origin "$GITHUB_PAGES_BRANCH"
+# git -C "$GITHUB_PAGES_DIR" config user.email "travis@users.noreply.github.com"
+# git -C "$GITHUB_PAGES_DIR" config user.name travis
+# git -C "$GITHUB_PAGES_DIR" config core.sshCommand 'ssh -i github_deploy_key'
+# git -C "$GITHUB_PAGES_DIR" add .
+# git -C "$GITHUB_PAGES_DIR" status
+# git -C "$GITHUB_PAGES_DIR" commit -m "Published by Travis"
+# git -C "$GITHUB_PAGES_DIR" push origin "$GITHUB_PAGES_BRANCH"
